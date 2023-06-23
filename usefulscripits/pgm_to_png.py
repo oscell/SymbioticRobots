050e19526169e0f8e5f485604dd4452d3030c840
oscell <oscar.meunierpbs@gmai.com>
2023-06-23 16:13:30 +0100

    added working updating minimap

--- usefulscripits/pgm_to_png.py
+++ usefulscripits/pgm_to_png.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Open the .pgm file
-img = Image.open('../assets/mymap.pgm')
+img = Image.open('../assets/mymap1.pgm')
 
 # Convert and save as .jpg
-img.save('../assets/mymap.png')
+img.save('../assets/mymap1.png')
