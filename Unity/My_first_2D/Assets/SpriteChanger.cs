58a126fee83988a9308b3c929d44477d3f73e275
oscell <oscar.meunierpbs@gmai.com>
2023-06-23 16:13:30 +0100

    added working updating minimap

--- Unity/My_first_2D/Assets/SpriteChanger.cs
+++ Unity/My_first_2D/Assets/SpriteChanger.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class SpriteChanger : MonoBehaviour
 {
-    public Sprite[] sprites; // Array to store your sprites
+    public Sprite sprites; // Array to store your sprites
     public SpriteRenderer spriteRenderer; // Sprite Renderer component
     public float changeInterval = 3.0f; // Interval time in seconds
 
-    private int currentSpriteIndex = 0; // Keep track of the current sprite
-
     void Start()
     {
-        if (sprites.Length > 0 && spriteRenderer != null)
-        {
-            // Start the ChangeSprite coroutine
+
             StartCoroutine(ChangeSprite());
-        }
     }
 
     IEnumerator ChangeSprite()
@@ -24,16 +19,7 @@ public class SpriteChanger : MonoBehaviour
         while (true)
         {
             // Set the sprite
-            spriteRenderer.sprite = sprites[currentSpriteIndex];
-
-            // Increment the index
-            currentSpriteIndex++;
-
-            // If we've gone past the end of the array, loop back to the start
-            if (currentSpriteIndex >= sprites.Length)
-            {
-                currentSpriteIndex = 0;
-            }
+            spriteRenderer.sprite = sprites;
 
             // Wait for the specified interval
             yield return new WaitForSeconds(changeInterval);
