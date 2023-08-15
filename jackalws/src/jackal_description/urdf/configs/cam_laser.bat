cb9cbbcd1ea5e9c404b3c442e3612ed913faa7d2
oscell <82882938+oscell@users.noreply.github.com>
2023-08-15 15:00:06 +0100

    removed stuff

--- jackalws/src/jackal_description/urdf/configs/cam_laser.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM The front_laser configuration of Jackal is sufficient for
-REM basic gmapping and navigation. It is mostly the default
-REM config, but with a SICK LMS100 series LIDAR on the front,
-REM pointing forward.
-
-set JACKAL_LASER=1
-set JACKAL_URDF_EXTRAS=/home/oscell/jackal_ws/src/jackal_description/urdf/realsense.urdf.xacro
